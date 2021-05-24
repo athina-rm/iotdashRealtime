604124f5e383dfc12bb3b52dd6251bc5d190b868
athina <athinamannaraprayil@gmail.com>
2021-05-24 19:22:25 +0200

    frontend start

--- IoTDash_Realtime/src/main/resources/static/app.js
+++ IoTDash_Realtime/src/main/resources/static/app.js
@@ -4,7 +4,11 @@ ws = new WebSocket('ws://localhost:80/device');
 
 ws.onmessage = function(event) {
     var deviceStatus = document.getElementById("deviceStatus");
-    var message = event.data;
-    deviceStatus.innerHTML = message;
+    var message = JSON.parse(event.data);
+    var tempValue=document.getElementById("tempValue");
+    tempValue.innerHTML=message.temp;
+    var humidity=document.getElementById("humidityValue");
+    humidity.innerHTML=message.humidity;
+    deviceStatus.innerHTML = event.data;
 };
 
